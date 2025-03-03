7aa5a991acf966727fb3a25308d971b2ac38ee1b
Renan <renandiutida@gmail.com>
2025-03-03 17:44:06 -0300

    LeituraDeDados.java: Adicionando métodos para leitura de dados do tipo int e float

--- src/main/java/org/example/LeituraDeDados.java
+++ src/main/java/org/example/LeituraDeDados.java
@@ -1,4 +1,33 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class LeituraDeDados {
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static int lerInteiro(String mensagem) {
+        while (true) {
+            try {
+                System.out.print(mensagem);
+                return scanner.nextInt();
+            } catch (Exception e) {
+                System.out.println("Erro: Digite um número válido.");
+            } finally {
+                scanner.nextLine();
+            }
+        }
+    }
+
+    public static double lerDouble(String mensagem) {
+        while (true) {
+            try {
+                System.out.print(mensagem);
+                return scanner.nextDouble();
+            } catch (Exception e) {
+                System.out.println("Erro: Digite um número válido.");
+            } finally {
+                scanner.nextLine();
+            }
+        }
+    }
 }
