dd700da5ddae01747b9ad76dd9ff4ba64fe0c720
Renan <renandiutida@gmail.com>
2025-03-03 16:37:35 -0300

    Restaurante.java: Mantento uma estrutura e organizalcao para quando o codigo comecar a rodar

--- src/main/java/org/example/Restaurante.java
+++ src/main/java/org/example/Restaurante.java
@@ -1,4 +1,33 @@
 package org.example;
 
+import java.util.*;
+
 public class Restaurante {
+    private List<Prato> cardapio;
+    private List<Pedido> pedidos;
+
+    public Restaurante() {
+        this.cardapio = new ArrayList<>();
+        this.pedidos = new ArrayList<>();
+    }
+
+    public void adicionarPrato(Prato prato) {
+        cardapio.add(prato);
+    }
+
+    public void removerPrato(Prato prato) {
+        cardapio.remove(prato);
+    }
+
+    public void criarPedido(Pedido pedido) {
+        pedidos.add(pedido);
+    }
+
+    public List<Prato> getCardapio() {
+        return cardapio;
+    }
+
+    public List<Pedido> getPedidos() {
+        return pedidos;
+    }
 }
