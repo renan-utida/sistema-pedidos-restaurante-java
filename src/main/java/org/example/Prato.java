ad1782a60159aa51e7cfa8978f1ac566fee80d30
Renan <renandiutida@gmail.com>
2025-03-03 16:19:37 -0300

    Prato.java: Desenvolvendo  e estruturando código

--- src/main/java/org/example/Prato.java
+++ src/main/java/org/example/Prato.java
@@ -1,4 +1,34 @@
 package org.example;
 
 public class Prato {
+    private String nome;
+    private double preco;
+    private String descricao;
+
+    public Prato(String nome, double preco, String descricao) {
+        this.nome = nome;
+        this.preco = preco;
+        this.descricao = descricao;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    @Override
+    public String toString() {
+        return "Prato{" +
+                "nome='" + nome + '\'' +
+                ", preco=" + preco +
+                ", descricao='" + descricao + '\'' +
+                '}';
+    }
 }
